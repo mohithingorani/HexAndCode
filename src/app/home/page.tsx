f7e95dc2cd827d95eaccf0b9560b77087ba05d9c
Mohit Hingorani <Mohithingorani2003@gmail.com>
2024-09-09 04:46:57 +0530

    complete dashboard

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import { NavBar2 } from "../components/NavBar";
+import { Footer } from "../components/Footer";
 
 export default function Home() {
   return (
@@ -6,20 +8,15 @@ export default function Home() {
       <NavBar2 />
       <div className="grid grid-cols-6 h-full">
         <div className="col-span-1 h-full bg-[#0C0807] w-full">
-            <div className="flex flex-col justify-center items-start text-2xl gap-8 text-[#FFE4B0] p-8">
-                <button className="hover:underline">Home</button>
-                <button className="hover:underline">Dashboard</button>
-                <button className="hover:underline">My Courses</button>
-                <button className="hover:underline">Assignments</button>
-                <button className="hover:underline">Terminal</button>
-
-            </div>
-
-
-
-
+          <div className="flex flex-col justify-center items-start text-2xl gap-8 text-[#FFE4B0] p-8">
+            <button className="hover:underline">Home</button>
+            <button className="hover:underline">Dashboard</button>
+            <button className="hover:underline">My Courses</button>
+            <button className="hover:underline">Assignments</button>
+            <button className="hover:underline">Terminal</button>
+          </div>
         </div>
-        
+
         <div className="col-span-5 h-full  w-full">
           <div className="flex justify-end w-full pl-24 pt-8">
             <div className="bg-[#E0E0E0] rounded-l-full w-full ">
@@ -38,6 +35,17 @@ export default function Home() {
               </div>
             </div>
           </div>
+          <div className="pl-24 pt-8 flex justify-between items-center">
+            <div className="text-6xl font-bold ">Dashboard</div>
+            <div className="flex text-sm pr-24 gap-8 text-blue-800">
+              <button className="hover:underline">Set Your Goals Today</button>
+              <button className="hover:underline">Filter</button>
+            </div>
+          </div>
+          <div className="flex w-full justify-start items-start px-24 pt-8 pb-8">
+            <Image src={"/allphotos.svg"} width={"800"} height={"300"} alt="photos"/>
+          </div>
+          <Footer/>
         </div>
       </div>
     </div>
